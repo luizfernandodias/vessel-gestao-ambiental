4673d83c59535a1536e0b0bfb54ae13b0a185569
Luiz Fernando Dias <luizfernandodias6@gmail.com>
2019-09-09 01:40:01 -0300

    correção de Bug - Erro ao importar os componentes do angular/material - configurações iniciais do projeto

--- frontend/vessel-gestao-ambiental/src/main.ts
+++ frontend/vessel-gestao-ambiental/src/main.ts
@@ -3,10 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 
 import { environment } from './environments/environment';
+import { AppModule } from './app/app.module';
 
 if (environment.production) {
   enableProdMode();
 }
 
-//platformBrowserDynamic().bootstrapModule(AppModule)
-//  .catch(err => console.error(err));
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
