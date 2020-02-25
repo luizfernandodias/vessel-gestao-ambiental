bb441828f6c1d2ae29c2895ea9df7250e7167445
unknown <luizfernandodias6@gmail.com>
2020-02-24 23:35:53 -0300

    Criação na estrutura do projeto

---
+++ backend/src/main/java/br/com/vessel/VesselApplication.java
@@ -0,0 +1,13 @@
+package br.com.vessel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VesselApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VesselApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/vessel/VesselApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vessel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VesselApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
