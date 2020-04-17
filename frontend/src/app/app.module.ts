f81520860c4e6778d937098a98382cd831df4935
luiz.dias <Plk402>
2020-04-17 09:30:50 -0300

    Criação da tela principal -frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,14 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { AutenticacaoModule } from './autenticacao/autenticacao.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AutenticacaoModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/autenticacao/autenticacao.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LoginComponent } from './login/login.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
+
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatInputModule,
+    BrowserAnimationsModule,
+    MatButtonModule
+  ],
+  exports: [
+    LoginComponent
+  ]
+})
+export class AutenticacaoModule { }
